cb70d99b893f39678bec672d7546d6a295090bc5
Agata <agata.bednarek.z@gmail.com>
2017-11-28 13:51:55 +0100

    Matematyka dzialania

--- Cwiczenie1/Cwiczenie1/MathematicsTest.cs
+++ Cwiczenie1/Cwiczenie1/MathematicsTest.cs
@@ -9,18 +9,37 @@ namespace Cwiczenie1
 {
     public class MathematicsTest
     {
-        [Fact]
-        public void Add_returns_sum_of_given_values()
+        [Theory]
+        [InlineData(10,20,30)]
+        [InlineData(10, 100, 110)]
+        [InlineData(5, 20, 25)]
+        [InlineData(5, 5, 10)]
+        [InlineData(8, 4, 12)]
+        [InlineData(10, 2, 12)]
+        public void TheoryExample(double x, double y, double expected)
         {
             // arrage
             var math = new Matematyka();
 
             // act
-            var result = math.Add(10, 20);
+            var result = math.Add(x, y);
 
             // assert
-            Assert.Equal(30, result);
+            Assert.Equal(expected, result);
         }
+        [Fact]
+        public void Add2_returns_sum_of_given_values()
+        {
+            // arrage
+            var math = new Matematyka();
+
+            // act
+            var result = math.Add(10, 100);
+
+            // assert
+            Assert.Equal(110, result);
+        }
+
 
         [Fact]
         public void Substract_returns_quotient_of_given_values()
@@ -29,10 +48,10 @@ namespace Cwiczenie1
             var math = new Matematyka();
 
             // act
-            var result = math.Substract(10, 20);
+            var result = math.Substract(5, 20);
 
             // assert
-            Assert.Equal(-10, result);
+            Assert.Equal(-15, result);
         }
 
         [Fact]
@@ -56,7 +75,7 @@ namespace Cwiczenie1
 
             // act
             var result = math.Division(8, 4);
-
+            
             // assert
             Assert.Equal(2, result);
         }
